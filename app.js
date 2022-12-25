df434e49db6e0e7cc08915bced3556f5e03a447a
Chily <cpo@appvestor.com>
2022-12-25 09:53:45 +0100

    Attack functionality added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function getRandomValue(max, min) {
-  return Math.floor(random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
@@ -9,7 +9,18 @@ const app = Vue.createApp({
       monsterHealth: 100
     };
   },
+  //to add inline styling dynamically
+  computed: {
+    monsterBarStyle() {
+      return {width: this.monsterHealth + '%'};
+    },
+    playerBarStyle() {
+      return {width: this.playerHealth + '%'};
+    }
+  },
   methods: {
+    //to attack the monster with random damage
+    //in return the monster attacks the player with random damage
     attackMonster() {
       const attackValue = getRandomValue(12, 5);
       this.monsterHealth  -= attackValue;
